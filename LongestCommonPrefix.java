9411a6eca61d70f5f030e301a49953f8c282deec
daichi <DevelopmentExample22.gmail.com>
2025-11-09 17:40:26 +0900

    LongestCommonPrefix.java 2025/11/09  各行にコメントを追加

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -6,20 +6,23 @@ public class LongestCommonPrefix {
         if(strs == null || strs.length == 0) {
             throw new IllegalArgumentException("配列が空です。");
         }
-
+        // 最初の文字列を prefix として仮置きする
         String prefix = strs[0];
-
+        // 配列の2番目以降の文字列と順番に比較する
         for(int i = 1; i < strs.length; i++) {
             String current = strs[i];
 
             int j = 0;
             int minLength = Math.min(prefix.length(), current.length());
 
+            // 現在の文字列と prefix を 0 文字目から順に比較して一致する長さをカウント
             while(j < minLength && prefix.charAt(j) == current.charAt(j)) {
                 j++;
             }
+            // prefix を一致する部分までに短縮
             prefix = prefix.substring(0, j);
 
+            // 途中で共通部分がなくなったら (prefix.isEmpty()) ループを終了
             if(prefix.isEmpty()) {
                 break;
             }
