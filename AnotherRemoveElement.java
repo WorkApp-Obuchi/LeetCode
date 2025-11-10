2326b4826971d4ac55d8acd0a9d1c876487c1660
daichi <DevelopmentExample22.gmail.com>
2025-11-11 00:40:44 +0900

    AnotherRemoveElement.java 2025/11/10  初回コミット

---
+++ AnotherRemoveElement.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class AnotherRemoveElement {
+    public int removeElement(int[] nums, int val) {
+
+        if(nums == null || nums.length == 0) return 0;
+
+        int k = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        RemoveElement re = new RemoveElement();
+
+        int[] nums1 = {3,2,2,3};
+        int val1 = 3;
+        int result1 = re.removeElement(nums1, val1);
+        System.out.println(result1); // output : 2
+
+        int[] nums2 = {0,1,2,2,3,0,4,2};
+        int val2 = 2;
+        int result2 = re.removeElement(nums2, val2);
+        System.out.println(result2); // output : 5
+    }
+}
