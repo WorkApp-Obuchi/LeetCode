a92ffe2c00b7b987e2edbc4463577355f5623d91
daichi <DevelopmentExample22.gmail.com>
2025-11-27 22:38:50 +0900

    ContainsDuplicate.java  2025/11/27 バグとなる箇所を修正

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -10,18 +10,28 @@ public class ContainsDuplicate {
         Map<Integer, Integer> map = new HashMap<>();
         for(int num : nums) {
             if(map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
+                // 2回以上出現する場合にはtrueを返せばいいので、新たに追加する必要はない
+                // 出現回数を数える必要がそもそもない
+                //（重複があるかどうかだけなら、1回出た時点で return true でよい）
+                //map.put(num, map.get(num) + 1); // 不要なので削除
+                return true;// 追加修正
             } else {
                 map.put(num, 1);
             }
         }
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if(entry.getValue() >= 2) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        // 以下の実装はバグになる
+        /*
+            例えば[1,2,1] の場合
+             map は {1=2, 2=1} のようになるけど
+            たまたま 2 が最初に取り出されたら value=1 → false を返してしまう
+         */
+//        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//            if(entry.getValue() >= 2) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
         return false;
     }
 
