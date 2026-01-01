461758a3ec8492c4328d9b5a1ed3dc09bfd3831e
daichi <DevelopmentExample22.gmail.com>
2026-01-02 00:19:33 +0900

    ReNumbersOfGoodPairs.java  2026/01/01 初回コミット(NumbersOfGoodPairs.javaの復習）

---
+++ HashMapSet/ReNumbersOfGoodPairs.java
@@ -0,0 +1,41 @@
+package LeetCode.HashMapSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReNumbersOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        if(nums == null || nums.length <= 0) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for(int num : nums) {
+            // ① 過去の出現回数を取得（自分自身は含まない）
+            int freq = map.getOrDefault(num, 0);
+            // ② 過去の出現回数分だけペアを作る(これまでの累計を出す、
+            // freqは各値（1,2,3など）の数をfor文でループした数の回数だけ各ループで表示させるため）
+            count += freq;
+            // ③ 自分自身を含めた回数に更新(①では過去の回数のみ取得しているため、今回のループについては含めていないため+1をする）
+            map.put(num, freq + 1);
+        }
+    return count;
+    }
+    public static void main(String[] args) {
+        NumbersOfGoodPairs solution = new NumbersOfGoodPairs();
+
+        // testcase1
+        int[] nums1 = {1,2,3,1,1,3}; // output : 4
+        System.out.println("良いペアの回数 : " + solution.numIdenticalPairs(nums1));
+
+
+        // testcase2
+        int[] nums2 = {1,1,1,1}; // output : 6
+        System.out.println("良いペアの回数 : " + solution.numIdenticalPairs(nums2));
+
+
+        // testcase3
+        int[] nums3 = {1,2,3}; // output : 0
+        System.out.println("良いペアの回数 : " + solution.numIdenticalPairs(nums3));
+    }
+}
