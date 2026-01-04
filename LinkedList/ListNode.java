6ab8cafe2a26d562128567d31ed30d9afe4bfd6e
daichi <DevelopmentExample22.gmail.com>
2026-01-04 13:51:39 +0900

    ListNode.java  2026/01/03 初回コミット

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -1,4 +1,17 @@
 package LeetCode.LinkedList;
 
 public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
 }
