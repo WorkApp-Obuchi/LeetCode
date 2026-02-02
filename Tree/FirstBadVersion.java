c25f6f80b5dbfda890fca64d25eaae51ebe1016b
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-03 00:07:05 +0900

    FirstVadVersion.java   2026/02/02 初回コミット

--- Tree/FirstBadVersion.java
+++ Tree/FirstBadVersion.java
@@ -1,4 +1,19 @@
 package LeetCode.Tree;
 
-public class FirstBadVersion {
+public class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if(isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
