0160651d14430f14b857c5702ea006251a102ee2
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-03 23:07:52 +0900

    FirstBadVersion.java   2026/02/02 初回コミット

--- Tree/FirstBadVersion.java
+++ Tree/FirstBadVersion.java
@@ -7,7 +7,7 @@ public class FirstBadVersion extends VersionControl {
 
         while(left < right) {
             int mid = left + (right - left) / 2;
-            
+
             if(isBadVersion(mid)) {
                 right = mid;
             } else {
