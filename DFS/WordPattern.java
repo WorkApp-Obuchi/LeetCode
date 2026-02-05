0189098b406d1a3918bf126748f20b9ac10fb730
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-05 22:56:16 +0900

    WordPattern.java   2026/02/05 初回コミット

---
+++ DFS/WordPattern.java
@@ -0,0 +1,41 @@
+package LeetCode.DFS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        // 単語配列を作成する
+        String[] words = s.split(" ");
+
+        // 長さチェック
+        if(pattern.length() != words.length) return false;
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for(int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            // 文字→単語チェック
+            if(charToWord.containsKey(c)) {
+                if(!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(c, word);
+            }
+
+            // 単語→文字チェック
+            if(wordToChar.containsKey(word)) {
+                if(wordToChar.get(word) != c) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, c);
+            }
+        }
+        return true;
+    }
+}
