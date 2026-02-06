64af6913049f7d5c35d6e724c26402ba47a42e93
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-06 17:55:49 +0900

    ReGuessNumber.java   2026/02/06  初回コミット

---
+++ DFS/ReGuessNumber.java
@@ -0,0 +1,22 @@
+package LeetCode.DFS;
+
+public class ReGuessNumber extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result == -1) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
