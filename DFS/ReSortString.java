2b68019a88e83f6ec4e14b294f2ac47a36b7c9bf
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-08 03:58:11 +0900

    ReSortString.java  2026/02/08  初回コミット

---
+++ DFS/ReSortString.java
@@ -0,0 +1,33 @@
+package LeetCode.DFS;
+
+public class ReSortString {
+    public String sortString(String s) {
+        // ①カウント配列を作成する
+        int[] count = new int[26];
+        for(char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        // ②ルールに従って文字列を構築
+        StringBuilder result = new StringBuilder();
+
+        while(result.length() < s.length()) {
+
+            // 昇順走査a→z
+            for(int i = 0; i < s.length(); i++) {
+                if(count[i] > 0) {
+                    result.append((char) ('a' + i));
+                    count[i]--;
+                }
+            }
+            // 降順走査z→a
+            for(int i = 25; i >= 0; i--) {
+                if(count[i] > 0) {
+                    result.append((char) ('a' + i));
+                    count[i]--;
+                }
+            }
+        }
+    return result.toString();
+    }
+}
