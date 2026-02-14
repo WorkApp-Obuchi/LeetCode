adfa36134d48fd55e5edcced8ece76d63ef5fe2f
WorkApp-Obuchi <daichi07070202@gmail.com>
2026-02-14 14:44:46 +0900

    ReReReSortString.java  2026/02/14  初回コミット

---
+++ DFS/ReReReSortString.java
@@ -0,0 +1,29 @@
+package LeetCode.DFS;
+
+public class ReReReSortString {
+    public String sortString(String s) {
+        int[] count = new int[26];
+        for(char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(result.length() < s.length()) {
+            // 昇順
+            for(int i = 0; i < 26; i++) {
+                if(count[i] > 0) {
+                    result.append((char) ('a' + 1));
+                    count[i]--;
+                }
+            }
+            // 降順
+            for(int i = 25; i >- 0; i--) {
+                if(count[i] > 0) {
+                    result.append((char) ('a' + 1));
+                    count[i]--;
+                }
+            }
+            return result.toString();
+        }
+    }
+}
